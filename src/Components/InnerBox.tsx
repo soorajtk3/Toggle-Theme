cf8f543f314351698de483ca05494a106140d794
sooraj vijayan <soorajtk3@gmail.com>
2024-03-16 13:21:12 +0530

    update code

--- src/Components/InnerBox.tsx
+++ src/Components/InnerBox.tsx
@@ -3,8 +3,10 @@ import { ThemeContext } from "./Panel";
 
 const InnerBox=()=>{
     const theme = useContext(ThemeContext);
+  
     
     return(
+        
         <div className={`innerDiv ${theme === 'light' ? 'lightMode' : 'darkMode'}`}>
         <h4>Welcome</h4>
         <button className={`btn ${theme === 'light' && 'lightBtn'}`} >Sign up</button>
